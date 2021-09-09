5ce4707e53a664026b07375cc04f0f661bc8771a
Đỗ Tuấn Kiệt <tuankiet141199@gmail.com>
2021-09-10 00:35:36 +0700

    transfer data into product management table

--- pages/admin/product-management.vue
+++ pages/admin/product-management.vue
@@ -1,5 +1,5 @@
 <template>
-    <div style="max-width:80%;margin:0 auto">
+    <div style="max-width: 80%; margin: 0 auto">
         <el-select
             v-model="selectKey"
             class="w-25 my-4"
@@ -17,35 +17,36 @@
                 :value="option.key"
             />
         </el-select>
-        <el-table
-            :data="tableData"
-            style="width: 100%"
-        >
-            <el-table-column
-                type="index"
-                width="50"
-            />
-            <el-table-column
-                prop="name"
-                label="Name"
-            />
-
-            <el-table-column
-                prop="parentName"
-                label="Parent name"
-            />
-
-            <el-table-column
-                fixed="right"
-                label="Operations"
-            >
+        <el-table :data="tableData" style="width: 100%">
+            <el-table-column fixed label="Created Date">
                 <template slot-scope="scope">
-                    <el-button type="text" @click.native.prevent="editRow(scope.row.id)">
-                        Edit
-                    </el-button>
-                    <el-button type="text" @click.native.prevent="deleteRow(scope.$index, tableData)">
-                        Remove
-                    </el-button>
+                    <span>{{ formatDate(scope.row.createdAt) }}</span>
+                </template>
+            </el-table-column>
+
+            <el-table-column fixed label="Expired Date">
+                <template slot-scope="scope">
+                    <span>{{ formatDate(scope.row.expiredAt) }}</span>
+                </template>
+            </el-table-column>
+
+            <el-table-column fixed prop="name" label="Product Name" />
+
+            <el-table-column fixed prop="status" label="Status" />
+
+            <el-table-column fixed="right" label="Operations">
+                <template slot-scope="scope">
+                    <b-button-group size="sm">
+                        <b-button squared variant="info" style="font-weight: bold; color:white" @click="handleClick">
+                            Detail
+                        </b-button>
+                        <b-button squared variant="success" style="font-weight: bold; color:white" @click.native.prevent="editRow(scope.row.id)">
+                            Edit
+                        </b-button>
+                        <b-button squared variant="danger" style="font-weight: bold; color:white" @click.native.prevent="deleteRow(scope.$index, tableData)">
+                            Remove
+                        </b-button>
+                    </b-button-group>
                 </template>
             </el-table-column>
         </el-table>
@@ -68,40 +69,50 @@
             <el-input placeholder="Please input" />
             <span slot="footer" class="dialog-footer">
                 <el-button @click="dialogVisible = false">Cancel</el-button>
-                <el-button type="primary" @click="dialogVisible = false">Confirm</el-button>
+                <el-button
+                    type="primary"
+                    @click="dialogVisible = false"
+                >Confirm</el-button>
             </span>
         </el-dialog>
     </div>
 </template>
 
 <script lang="ts">
+import momment from 'moment';
+import { productService } from '~/services/product';
 export default {
     middleware: ['authentication'],
     data() {
         return {
-            tableData: [
-                {
-                    id: '1',
-                    name: 'abc',
-                    parentName: 'a'
-                }
-            ],
+            tableData: [],
             options: [
-                { key: 1, name: 'Seller' },
-                { key: 2, name: 'Bidder' },
-                { key: 3, name: 'Upgrage request' }
+                { key: 'mobile', name: 'Điện thoại di động' }, // test
+                { key: 'laptop', name: 'Máy tính xách tay' }, // test
+                { key: 3, name: 'Upgrage request' },
             ],
-            selectKey: '',
+            selectKey: '' as any,
             page: 1,
             total: 10,
-            perPage: 5,
-            loadingRemote: false,
-            dialogVisible: false
+            perPage: 10,
+            loadingRemote: false
         };
+    },
+    mounted() {
+        this.$nextTick(async () => {
+            this.$nuxt.$loading.start();
+            await this.getProducts();
+            this.$nuxt.$loading.finish();
+        });
     },
     methods: {
         handleClick() {
             console.log('click');
+        },
+        handleSelect() {
+            this.$nuxt.$loading.start();
+            if (this.selectKey !== 3) this.getProducts(this.selectKey);
+            this.$nuxt.$loading.finish();
         },
         remoteMethod(query: string) {
             console.log(query);
@@ -109,13 +120,29 @@ export default {
         handleChangePage() {
             console.log(this.page);
         },
-        deleteRow(index:number, rows:any) {
+        deleteRow(index: number, rows: any) {
             rows.splice(index, 1);
         },
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         editRow(id: string) {
             this.dialogVisible = true;
-        }
-    }
+        },
+        async getProducts(key: string = '') {
+            const query = `roleId=${key}&skip=${(this.page - 1) * this.perPage}`;
+            const result = await productService.findProduct(query).catch((error) => {
+                this.$notify.error({
+                    title: 'Error',
+                    message: error.message || 'Cannot get client!',
+                });
+            });
+            if (result) {
+                this.total = result.pagination.total;
+                this.tableData = result.data;
+            }
+        },
+        formatDate(date: any) {
+            return momment(date).format('k:mm D-M-Y');
+        },
+    },
 };
 </script>
